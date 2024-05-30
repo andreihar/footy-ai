184f3e2ee7d35910a6760a7d52a1720693832756
andreihar <95883512+andreihar@users.noreply.github.com>
2024-05-30 01:10:20 -0700

    Changed backend to use production server

--- server/app.py
+++ server/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 
@@ -20,4 +21,5 @@ def predict():
   return jsonify(result)
 
 if __name__ == '__main__':
-  app.run(debug=True)
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port, debug=True)
