770781a7c0882322e78714d418d9b6d1eb6d810f
andreihar <95883512+andreihar@users.noreply.github.com>
2024-07-11 22:55:59 -0700

    Fixed ordering of matches in bracket when the team is unknown

--- client/src/app/knockout/page.tsx
+++ client/src/app/knockout/page.tsx
@@ -8,7 +8,7 @@ import Match from '../types/match';
 import './style.scss';
 
 const MatchBracket = ({ match }: { match: Match; }) => {
-  const { getFlag } = useCountryFlags();
+  const { getFlag, getHistoricalName } = useCountryFlags();
 
   return (
     <Box className="match" display="flex" position="relative" flexDirection="column">
@@ -16,21 +16,23 @@ const MatchBracket = ({ match }: { match: Match; }) => {
         <Box pr={0.5} position="relative" display="inline-block" height="100%">
           <Avatar variant="square" alt="?" className="angled-image" src={getFlag(match.home_team)} />
         </Box>
-        <Typography className="name" textTransform="uppercase" letterSpacing={0.5}>{match.home_team}</Typography>
+        <Typography className="name" textTransform="uppercase" letterSpacing={0.5}>{getHistoricalName(match.home_team)}</Typography>
         <Typography className="score">{match.scorePrediction[0]}</Typography>
       </Box>
       <Box className={`match-bottom team ${match.home_score_total < match.away_score_total ? 'winner' : ''}`}>
         <Box pr={0.5} position="relative" display="inline-block" height="100%">
           <Avatar variant="square" alt="?" className="angled-image" src={getFlag(match.away_team)} />
         </Box>
-        <Typography className="name" textTransform="uppercase" letterSpacing={0.5}>{match.away_team}</Typography>
+        <Typography className="name" textTransform="uppercase" letterSpacing={0.5}>{getHistoricalName(match.away_team)}</Typography>
         <Typography className="score">{match.scorePrediction[1]}</Typography>
       </Box>
       {match.stage !== 'Third-place play-off' && <>
-        <Box className="match-lines">
-          <Box className="line one" />
-          <Box className="line two" />
-        </Box>
+        {match.stage !== 'Final' && (
+          <Box className="match-lines">
+            <Box className="line one" />
+            <Box className="line two" />
+          </Box>
+        )}
         <Box className="match-lines alt">
           <Box className="line one" />
         </Box>
@@ -43,25 +45,23 @@ const KnockoutPage = () => {
   const { data } = useStats();
 
   const sortTournamentMatches = (data: Match[]): { [stage: string]: Match[]; } => {
-    const stages = ['Round of 16', 'Quarter-finals', 'Semi-finals', 'Final'];
-    const sortedMatchesByStage: { [stage: string]: Match[]; } = stages.reduce<{ [key: string]: Match[]; }>((acc, stage) => {
+    const stages = ['Round of 16', 'Quarter-finals', 'Semi-finals', 'Final', 'Third-place play-off'];
+    let teamWeights: { [team: string]: number; } = {};
+    const sortedMatchesByStage = stages.reduce<{ [stage: string]: Match[]; }>((acc, stage) => {
       acc[stage] = [];
       return acc;
     }, {});
 
     for (let i = stages.length - 1; i >= 0; i--) {
-      const matches = data.filter(match => match.stage === stages[i]);
-      if (stages[i] === 'Final') {
-        sortedMatchesByStage[stages[i]] = matches;
-        continue;
-      }
-      const winTeams = sortedMatchesByStage[stages[i + 1]]?.flatMap(match => [match.home_team, match.away_team].filter(team => team !== '?')) || [];
-      const sortedMatches = matches.sort((a, b) => {
-        const aIndexHome = winTeams.indexOf(a.home_team) !== -1 ? winTeams.indexOf(a.home_team) : Infinity;
-        const aIndexAway = winTeams.indexOf(a.away_team) !== -1 ? winTeams.indexOf(a.away_team) : Infinity;
-        const bIndexHome = winTeams.indexOf(b.home_team) !== -1 ? winTeams.indexOf(b.home_team) : Infinity;
-        const bIndexAway = winTeams.indexOf(b.away_team) !== -1 ? winTeams.indexOf(b.away_team) : Infinity;
-        return Math.min(aIndexHome, aIndexAway) - Math.min(bIndexHome, bIndexAway);
+      const stageMatches = data.filter(match => match.stage === stages[i]);
+      const sortedMatches = stageMatches.sort((a, b) => {
+        const aWeight = (teamWeights[a.home_team] || 0) + (teamWeights[a.away_team] || 0);
+        const bWeight = (teamWeights[b.home_team] || 0) + (teamWeights[b.away_team] || 0);
+        return aWeight - bWeight;
+      });
+      sortedMatches.forEach((match, index) => {
+        if (match.home_team !== '?') teamWeights[match.home_team] = (index * 2) + 1;
+        if (match.away_team !== '?') teamWeights[match.away_team] = (index * 2) + 2;
       });
       sortedMatchesByStage[stages[i]] = sortedMatches;
     }
