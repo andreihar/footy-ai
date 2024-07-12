1c23b09c03f05198e579b7d11acb3718fd2acf2d
andreihar <95883512+andreihar@users.noreply.github.com>
2024-07-11 20:40:47 -0700

    Added tournament bracket

--- client/src/app/knockout/page.tsx
+++ client/src/app/knockout/page.tsx
@@ -1,127 +1,88 @@
 'use client';
-import { Avatar, Box, Button, CardContent, Typography, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+import { Avatar, Box, Typography } from '@mui/material';
 import PageContainer from '@/app/components/container/PageContainer';
-import DashboardCard from '@/app/components/shared/DashboardCard';
-import CheckCircleIcon from '@mui/icons-material/CheckCircle';
-import CancelIcon from '@mui/icons-material/Cancel';
-import HelpIcon from '@mui/icons-material/Help';
 
 import { useStats } from '../../utils/StatsContext';
 import useCountryFlags from '../../utils/countryUtils';
 import Match from '../types/match';
+import './style.scss';
 
-const MatchCard = ({ match }: { match: Match; }) => {
+const MatchBracket = ({ match }: { match: Match; }) => {
   const { getFlag } = useCountryFlags();
 
-  const predictedOutcomeIndex = match.predictions.indexOf(Math.max(...match.predictions));
-  let predictedOutcome = "";
-  if (predictedOutcomeIndex === 0) predictedOutcome = "home";
-  else if (predictedOutcomeIndex === 1) predictedOutcome = "away";
-  else if (predictedOutcomeIndex === 2) predictedOutcome = "draw";
-
-  let correctOutcome = "unknown";
-  let correctScore = "unknown";
-
-  if (!isNaN(match.home_score_total) && !isNaN(match.away_score_total)) {
-    let actualOutcome = "";
-    if (match.home_score_total > match.away_score_total) actualOutcome = "home";
-    else if (match.home_score_total < match.away_score_total) actualOutcome = "away";
-    else if (match.home_score_total === match.away_score_total) actualOutcome = "draw";
-    correctOutcome = predictedOutcome === actualOutcome ? "correct" : "incorrect";
-    correctScore = (match.scorePrediction[0] === match.home_score_total && match.scorePrediction[1] === match.away_score_total) ? "correct" : "incorrect";
-  }
-
   return (
-    <DashboardCard>
-      <CardContent>
-        <Box display="flex" flexDirection="column" justifyContent="center" alignItems="center" mb={2}>
-          <Typography variant="h6">
-            {new Date(match.date).toLocaleString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })}
-            &nbsp; • &nbsp;
-            {new Date(match.date).toLocaleString(undefined, { hour: 'numeric', minute: 'numeric' })}
-          </Typography>
-          <Typography variant="h5" mt={2} >{match.stage}</Typography>
-          <Typography sx={{ textTransform: "uppercase" }} mt={2}>{match.stadium}, {match.city}</Typography>
+    <Box className="match" display="flex" position="relative" flexDirection="column">
+      <Box className={`match-top team ${match.home_score_total > match.away_score_total ? 'winner' : ''}`}>
+        <Box pr={0.5} position="relative" display="inline-block" height="100%">
+          <Avatar variant="square" alt="?" className="angled-image" src={getFlag(match.home_team)} />
         </Box>
-        <Box display="flex" justifyContent="center" alignItems="center">
-          {/* Home */}
-          <Box display="flex" flexDirection="column" alignItems="center" textAlign="center" marginRight={2}>
-            <Avatar alt="?" src={getFlag(match.home_team)} sx={{ width: 80, height: 80, marginBottom: 1, border: '0.5px solid lightgray' }} />
-            <Typography variant="h3">{match.home_team}</Typography>
-          </Box>
-          <Box display="flex" justifyContent="center" alignItems="center" sx={{ marginX: 4 }}>
-            <Box display="flex" flexDirection="column" alignItems="center" sx={{ mr: 2 }}>
-              <Typography variant="h1" component="span">{match.scorePrediction[0]}</Typography>
-              <Typography variant="body1" component="span">({match.home_score_total})</Typography>
-            </Box>
-            <Typography variant="h4" component="span" sx={{ mx: 2 }}>-</Typography>
-            <Box display="flex" flexDirection="column" alignItems="center" sx={{ ml: 2 }}>
-              <Typography variant="h1" component="span">{match.scorePrediction[1]}</Typography>
-              <Typography variant="body1" component="span">({match.away_score_total})</Typography>
-            </Box>
-          </Box>
-          {/* Away */}
-          <Box display="flex" flexDirection="column" alignItems="center" textAlign="center" marginLeft={2}>
-            <Avatar alt="?" src={getFlag(match.away_team)} sx={{ width: 80, height: 80, marginBottom: 1, border: '0.5px solid lightgray' }} />
-            <Typography variant="h3">{match.away_team}</Typography>
-          </Box>
+        <Typography className="name" textTransform="uppercase" letterSpacing={0.5}>{match.home_team}</Typography>
+        <Typography className="score">{match.scorePrediction[0]}</Typography>
+      </Box>
+      <Box className={`match-bottom team ${match.home_score_total < match.away_score_total ? 'winner' : ''}`}>
+        <Box pr={0.5} position="relative" display="inline-block" height="100%">
+          <Avatar variant="square" alt="?" className="angled-image" src={getFlag(match.away_team)} />
         </Box>
-        <Box mt={5} sx={{ width: '100%', bgcolor: 'grey.300', borderRadius: '10px', height: '24px', display: 'flex' }}>
-          <Box sx={{ bgcolor: 'primary.main', borderRadius: '6px 0 0 6px', width: `${match.predictions[0]}%`, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            <Typography variant="h6" color="white">{match.predictions[0]}%</Typography>
-          </Box>
-          {match.predictions[2] > 0 && (
-            <Box sx={{ bgcolor: 'grey.300', width: `${match.predictions[2]}%`, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-              <Typography variant="h6">{match.predictions[2]}%</Typography>
-            </Box>
-          )}
-          <Box sx={{ bgcolor: 'secondary.main', borderRadius: '0 6px 6px 0', width: `${match.predictions[1]}%`, display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            <Typography variant="h6">{match.predictions[1]}%</Typography>
-          </Box>
+        <Typography className="name" textTransform="uppercase" letterSpacing={0.5}>{match.away_team}</Typography>
+        <Typography className="score">{match.scorePrediction[1]}</Typography>
+      </Box>
+      {match.stage !== 'Third-place play-off' && <>
+        <Box className="match-lines">
+          <Box className="line one" />
+          <Box className="line two" />
         </Box>
-        <Box mt={3} display="flex" justifyContent="center" width="100%">
-          <Accordion disableGutters elevation={0} sx={{ width: '100%' }}>
-            <AccordionSummary
-              aria-controls="panel1a-content"
-              id="panel1a-header"
-              sx={{ justifyContent: 'center', display: 'flex', width: '100%' }}
-            >
-              <Button variant="contained" color="primary" component="div" sx={{ mx: 'auto' }}>
-                Prediction Results
-              </Button>
-            </AccordionSummary>
-            <AccordionDetails>
-              <Box display="flex" flexDirection="column" alignItems="center" width="100%">
-                <Box display="flex" mb={2}>
-                  {correctOutcome === "correct" && <CheckCircleIcon color="success" />}
-                  {correctOutcome === "incorrect" && <CancelIcon color="error" />}
-                  {correctOutcome === "unknown" && <HelpIcon sx={{ color: 'gray' }} />}
-                  <Typography ml={1}>Correct Outcome Prediction</Typography>
-                </Box>
-                <Box display="flex">
-                  {correctScore === "correct" && <CheckCircleIcon color="success" />}
-                  {correctScore === "incorrect" && <CancelIcon color="error" />}
-                  {correctScore === "unknown" && <HelpIcon sx={{ color: 'gray' }} />}
-                  <Typography ml={1}>Correct Score Prediction</Typography>
-                </Box>
-              </Box>
-            </AccordionDetails>
-          </Accordion>
+        <Box className="match-lines alt">
+          <Box className="line one" />
         </Box>
-      </CardContent>
-    </DashboardCard >
+      </>}
+    </Box>
   );
 };
 
 const KnockoutPage = () => {
   const { data } = useStats();
 
+  const sortTournamentMatches = (data: Match[]): { [stage: string]: Match[]; } => {
+    const stages = ['Round of 16', 'Quarter-finals', 'Semi-finals', 'Final'];
+    const sortedMatchesByStage: { [stage: string]: Match[]; } = stages.reduce<{ [key: string]: Match[]; }>((acc, stage) => {
+      acc[stage] = [];
+      return acc;
+    }, {});
+
+    for (let i = stages.length - 1; i >= 0; i--) {
+      const matches = data.filter(match => match.stage === stages[i]);
+      if (stages[i] === 'Final') {
+        sortedMatchesByStage[stages[i]] = matches;
+        continue;
+      }
+      const winTeams = sortedMatchesByStage[stages[i + 1]]?.flatMap(match => [match.home_team, match.away_team].filter(team => team !== '?')) || [];
+      const sortedMatches = matches.sort((a, b) => {
+        const aIndexHome = winTeams.indexOf(a.home_team) !== -1 ? winTeams.indexOf(a.home_team) : Infinity;
+        const aIndexAway = winTeams.indexOf(a.away_team) !== -1 ? winTeams.indexOf(a.away_team) : Infinity;
+        const bIndexHome = winTeams.indexOf(b.home_team) !== -1 ? winTeams.indexOf(b.home_team) : Infinity;
+        const bIndexAway = winTeams.indexOf(b.away_team) !== -1 ? winTeams.indexOf(b.away_team) : Infinity;
+        return Math.min(aIndexHome, aIndexAway) - Math.min(bIndexHome, bIndexAway);
+      });
+      sortedMatchesByStage[stages[i]] = sortedMatches;
+    }
+    return sortedMatchesByStage;
+  };
+
   return (
     <PageContainer title="Knockout Stage" description="List of matches and predictions for the Knockout Stage">
-      <>
-        {data.filter(match => match.home_team !== "?" && match.away_team !== "?" && !match.stage.startsWith("Group"))
-          .reverse().map((match, index) => <MatchCard key={index} match={match} />)}
-      </>
+      <Box display="flex" className="theme">
+        {Object.entries(sortTournamentMatches(data)).map(([stageName, matches]) => {
+          return (
+            matches.length > 0 && (
+              <Box className="column" key={stageName} display="flex" flexDirection="column" minHeight="100%" justifyContent="space-around" alignItems="center">
+                {matches.map((match, index) => (
+                  <MatchBracket key={`${stageName}-${index}`} match={match} />
+                ))}
+              </Box>
+            )
+          );
+        })}
+      </Box>
     </PageContainer>
   );
 };
