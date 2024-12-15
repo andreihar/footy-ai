8a4dd93c75090aae0f9f31332590bd7fcc8fc51d
andreihar <95883512+andreihar@users.noreply.github.com>
2024-12-15 01:57:05 -0800

    Added new font for Headers, added year generaton for Nations League

--- client/src/config.ts
+++ client/src/config.ts
@@ -3,14 +3,21 @@ export const host = process.env.NEXT_PUBLIC_URL
   ? `https://${process.env.NEXT_PUBLIC_URL}`
   : `http://localhost:${port}`;
 
-export const endYear = parseInt(process.env.NEXT_PUBLIC_END_YEAR || '2024', 10);
+export const endEuroYear = parseInt(process.env.NEXT_PUBLIC_END_EURO_YEAR || '2024', 10);
+export const endNationsYear = parseInt(process.env.NEXT_PUBLIC_END_NATIONS_YEAR || '2025', 10);
 
-export const generateYearsArray = (start: number, end: number, gap: number): number[] => {
-  const years = [];
-  for (let year = end; year >= start; year -= gap) {
-    years.push(year);
+export const generateYearsArray = (startEuro: number, startNations: number, endEuro: number, endNations: number): number[] => {
+  const euroYears = [];
+  const nationsYears = [];
+  for (let year = endEuro; year >= startEuro; year -= 4) {
+    euroYears.push(year);
   }
-  return years;
+  for (let year = endNations; year >= startNations; year -= 2) {
+    nationsYears.push(year);
+  }
+  const combinedYears = [...euroYears, ...nationsYears].sort((a, b) => b - a);
+
+  return combinedYears;
 };
 
-export const years = generateYearsArray(1960, endYear, 4);
+export const years = generateYearsArray(1960, 2019, endEuroYear, endNationsYear);
